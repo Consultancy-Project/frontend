d8fb22b5b4b64eba5efed163fe15dc88028c6fa5
Aidan McGuire <>
2021-06-06 12:41:23 -0600

    Add structure for services tests

---
+++ spec/services/be_service.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'Backend service' do
+  describe 'recent tweets' do
+    it 'returns an array of three tweets'
+  end
+  
+  describe 'recent news' do
+    it 'returns an array of three news article headlines and links'
+  end
+end
