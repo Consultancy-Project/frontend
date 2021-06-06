507c353038559dcb6449bbf2533522e61e46a164
Aidan McGuire <>
2021-06-06 12:41:54 -0600

    Add be_service

---
+++ app/services/be_service.rb
@@ -0,0 +1,23 @@
+class BeService
+  class << self
+    def recent_tweets
+      resp = conn.get("/tweets")
+      parse_json(resp)
+    end
+    
+    def recent_news
+      resp = conn.get("/news")
+      parse_json(resp)
+    end
+
+    private 
+    
+    def conn
+      Faraday.new(url: 'https://localhost:3000/api/v1')
+    end
+    
+    def parse_json(response)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+  end
+end
