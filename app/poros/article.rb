a0d6c716f109167516c1135dd7789ac18b909088
Aidan McGuire <>
2021-06-07 16:53:23 -0600

    Update Article poro

--- app/poros/article.rb
+++ app/poros/article.rb
@@ -1,8 +1,13 @@
 class Article
   attr_reader :headline,
-              :url
+              :url,
+              :image,
+              :summary
+              
   def initialize(data)
     @headline = data[:attributes][:headline]
     @url = data[:attributes][:url]
+    @image = data[:attributes][:image]
+    @summary = data[:attributes][:summary]
   end
 end
