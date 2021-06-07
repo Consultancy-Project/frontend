c62b5b6fa17e2f3129cb568abada44622fb7e140
Aidan McGuire <>
2021-06-07 16:56:29 -0600

    Update Tweet poro

--- app/poros/tweet.rb
+++ app/poros/tweet.rb
@@ -1,7 +1,9 @@
 class Tweet
-  attr_reader :tweet
+  attr_reader :tweet,
+              :username
 
   def initialize(data)
     @tweet = data[:attributes][:tweet]
+    @username = data[:attributes][:username]
   end
 end
