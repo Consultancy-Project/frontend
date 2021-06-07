8221d8fb2b749ead79ab10e5cf0ad3641373dd1d
Aidan McGuire <>
2021-06-07 17:05:08 -0600

    Add logout tests

--- spec/features/users/news_spec.rb
+++ spec/features/users/news_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'user dashboard' do
+RSpec.describe 'news page' do
   before :each do |test|
     @user = User.create!(name: 'test_user')
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user) unless test.metadata[:logged_out]
@@ -10,12 +10,12 @@ RSpec.describe 'user dashboard' do
     expect{ visit dashboard_path }.to raise_error(ActionController::RoutingError)
   end
 
-  xit 'does not allow a user who logged out to view the dashboard' do
+  xit 'does not allow a user who logged out to view the news page' do
     visit dashboard_path
     click_link "Logout"
     expect(current_path).to eq(dashboard_path)
 
-    visit dashboard_path
+    visit news_path
     expect{ visit dashboard_path }.to raise_error(ActionController::RoutingError)
   end
 end
