c1d3d098900ec9f019765e301b502cb02ef1b659
George Ho <george.h.w.ho@gmail.com>
2021-06-09 19:52:13 -0400

    [heroku] bootstrap js setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require bootstrap
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require jquery
-//= require jquery_ujs
 //= require_tree .
